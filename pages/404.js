e3d5bf66d2103d5dac9fce3787597291cf88f8dd
Adam <adminstrony@zs2lancut.pl>
2022-04-13 01:16:50 +0200

    update 404.js

--- pages/404.js
+++ pages/404.js
@@ -4,10 +4,7 @@ const NotFoundPage = () => {
   return (
     <Layout title="404:Nie znaleziono takiej strony">
       <div className="wrapper center min NotFoundPage">
-        <iframe
-          className="airtable-embed"
-          src="https://airtable.com/embed/shrQPgnjLXuHnwZpA?backgroundColor=purple"
-        ></iframe>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfICVfL8s16d5CK0MAJp57yPGtrdoLLBgpFu8W4g8Bfr1t_ug/viewform?embedded=true" />
       </div>
     </Layout>
   )
