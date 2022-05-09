9f11b2a669c83ce6a06fc3d70a5be34ed07ab1c6
Adam <Adammajcher20@gmail.com>
2022-05-09 12:10:10 +0200

    kalendarz wydarzen

--- pages/kalendarz-wydarzen.js
+++ pages/kalendarz-wydarzen.js
@@ -1,15 +1,14 @@
-import Layout from '../../templates/Layout'
+import Layout from '../templates/Layout'
 import Link from 'next/link'
 const Index = () => {
   return (
     <Layout title="Kalendarz Wydarzeń" description="Kalendarz Wydarzeń">
       <div className="wrapper center min">
-        <Link
-          href="https://archiwum2.zs2lancut.pl/index.php/o-nas/kalendarz-wydarzen"
-          passHref
-        >
-          Kalendarz Wydarzeń
-        </Link>
+        <iframe
+          className="kalendarz"
+          src="https://archiwum2.zs2lancut.pl/index.php/o-nas/kalendarz-wydarzen"
+          title="Kalendarz Wydarzen"
+        ></iframe>
       </div>
     </Layout>
   )
